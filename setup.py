98c6a06002091cf360624509ab4d1b375294902c
Nils Leif Fischer <nils.fischer@aei.mpg.de>
2020-01-24 15:22:49 +0100

    Init with setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='spec_diagnose',
+    version='0.0.1',
+    description="Utility for diagnosing SpEC simulations",
+    author="SXS collaboration",
+    url="https://black-holes.org",
+    packages=['spec_diagnose'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
