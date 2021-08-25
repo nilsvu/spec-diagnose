5945a2392550f122ad3b34479f71e034cc73b633
Harald Pfeiffer <harald.pfeiffer@aei.mpg.de>
2021-08-25 17:41:26 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,4 +14,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    # install_requires=['numpy',...']   <-- Leo recommends adding all dependencies here
 )
